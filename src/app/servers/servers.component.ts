156b4695ba5f7cee1b60ac3bc815f1c921a64cd8
RicardoFazzi <ricardofazzi007@gmail.com>
2017-04-28 10:46:49 -0300

    Some things learnt before assignment 1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //This selector is by tag
+  //selector: '[app-servers]', //This selector is by attribute
+  //selector: '.app-servers', //This selector is by class
+  /*template: `
+    <app-server></app-server>
+    <app-server></app-server>`,*/
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
