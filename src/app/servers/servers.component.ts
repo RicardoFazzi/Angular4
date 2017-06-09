016219544fc06579293b52bfe793a059edb0ca1b
RicardoFazzi <ricardofazzi007@gmail.com>
2017-06-09 15:33:37 -0300

    two way binding [(ngModel)]

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -26,7 +26,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created. Name was: ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
