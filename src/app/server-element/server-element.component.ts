771a07dfbe0a9114b43130e3ce23066faef38a9c
Ricardo Fazzi <ricardofazzi007@gmail.com>
2017-10-01 14:11:53 -0300

    [cmp-databinding-start] @Input(), @Output() & encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
+  /**
+   * ViewEncapsulation is about css. Each css file works only in each component thanks to encapsulation.
+   * Emulated is the default. Angular does some work to avoid browser issues.
+   * None is to avoid encapsulation, and css will apply to all components app wide.
+   * Native is to use the native encapsulation if browsers support it, but could have some problems using
+   * non supported browsers.
+   */
 })
 export class ServerElementComponent implements OnInit {
+
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
